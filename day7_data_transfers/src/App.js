702c2f60e96bdd377ee796054c094e28ddca1fbc
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-23 07:14:05 +0000

    create prop

--- day7_data_transfers/src/App.js
+++ day7_data_transfers/src/App.js
@@ -2,9 +2,15 @@ import {useState} from 'react'
 import Childcom from './Childcom'
 
 function App(){
+    const[childname,setChildname]=useState("default");
+
+    const receivename=(name)=>{
+        setChildname(name);
+    }
 return (
 <>
 <h1>Hi Im the react parent</h1>
+<Childcom setFun={receivename}/>
 
 </>
 );
