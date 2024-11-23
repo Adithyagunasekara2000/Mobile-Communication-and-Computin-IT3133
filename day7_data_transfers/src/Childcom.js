f36e3fd75b6bbffd23fd45dcc5f0d226bb03c15f
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-23 07:20:21 +0000

    correction

--- day7_data_transfers/src/Childcom.js
+++ day7_data_transfers/src/Childcom.js
@@ -2,7 +2,7 @@ export default function Childcom(props){
     return<>
     <h1>hey Im react child</h1>
     {
-        props.setfun("Im Adithya")
+        props.setFun("Im Adithya")
     }
     </>
 }
